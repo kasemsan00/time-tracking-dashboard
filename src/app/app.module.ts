97c568c642a66cde8e6f0c9c5e02e7c70cc24396
Kasemsan <kasemsan.cho@gmail.com>
2023-03-13 21:40:53 +0700

    dev

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,49 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+  private dashboardJSON = [
+    {
+      id: 0,
+      title: 'Work',
+      time: '32hrs',
+      active: 'Last Week - 36hrs',
+    },
+    {
+      id: 1,
+      title: 'Play',
+      time: '10hrs',
+      active: 'Last Week - 8hrs',
+    },
+    {
+      id: 2,
+      title: 'Study',
+      time: '4hrs',
+      active: 'Last Week - 7hrs',
+    },
+    {
+      id: 3,
+      title: 'Exercise',
+      time: '4hrs',
+      active: 'Last Week - 5hrs',
+    },
+    {
+      id: 4,
+      title: 'Social',
+      time: '5hrs',
+      active: 'Last Week - 10hrs',
+    },
+
+    {
+      id: 5,
+      title: 'Self Care',
+      time: '2hrs',
+      active: 'Last Week - 2hrs',
+    },
+  ];
+}
