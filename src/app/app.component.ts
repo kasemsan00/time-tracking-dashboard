f1a873c6e35b52250e05a1ac4c37c367e769bac7
Kasemsan <kasemsan.cho@gmail.com>
2023-03-13 21:57:29 +0700

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'time-tracking-dashboard';
+  title = "time-tracking-dashboard";
+  dashboardItems = [
+    {
+      id: 0,
+      title: "Work",
+      time: "32hrs",
+      active: "Last Week - 36hrs",
+    },
+    {
+      id: 1,
+      title: "Play",
+      time: "10hrs",
+      active: "Last Week - 8hrs",
+    },
+    {
+      id: 2,
+      title: "Study",
+      time: "4hrs",
+      active: "Last Week - 7hrs",
+    },
+    {
+      id: 3,
+      title: "Exercise",
+      time: "4hrs",
+      active: "Last Week - 5hrs",
+    },
+    {
+      id: 4,
+      title: "Social",
+      time: "5hrs",
+      active: "Last Week - 10hrs",
+    },
+
+    {
+      id: 5,
+      title: "Self Care",
+      time: "2hrs",
+      active: "Last Week - 2hrs",
+    },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
@@ -10,44 +10,4 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-  private dashboardJSON = [
-    {
-      id: 0,
-      title: 'Work',
-      time: '32hrs',
-      active: 'Last Week - 36hrs',
-    },
-    {
-      id: 1,
-      title: 'Play',
-      time: '10hrs',
-      active: 'Last Week - 8hrs',
-    },
-    {
-      id: 2,
-      title: 'Study',
-      time: '4hrs',
-      active: 'Last Week - 7hrs',
-    },
-    {
-      id: 3,
-      title: 'Exercise',
-      time: '4hrs',
-      active: 'Last Week - 5hrs',
-    },
-    {
-      id: 4,
-      title: 'Social',
-      time: '5hrs',
-      active: 'Last Week - 10hrs',
-    },
-
-    {
-      id: 5,
-      title: 'Self Care',
-      time: '2hrs',
-      active: 'Last Week - 2hrs',
-    },
-  ];
-}
+export class AppModule {}
