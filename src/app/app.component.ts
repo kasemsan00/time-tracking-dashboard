984c986e56e7d04d23e96879d930102194a0622c
Kasemsan <kasemsan.cho@gmail.com>
2023-03-15 22:10:39 +0700

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,30 +13,40 @@ export class AppComponent {
       title: "Work",
       time: "32hrs",
       active: "Last Week - 36hrs",
+      color: "hsl(15, 100%, 70%)",
+      icon: "../assets/images/icon-work.svg",
     },
     {
       id: 1,
       title: "Play",
       time: "10hrs",
       active: "Last Week - 8hrs",
+      color: "hsl(195, 74%, 62%)",
+      icon: "../assets/images/icon-play.svg",
     },
     {
       id: 2,
       title: "Study",
       time: "4hrs",
       active: "Last Week - 7hrs",
+      color: "hsl(348, 100%, 68%)",
+      icon: "../assets/images/icon-study.svg",
     },
     {
       id: 3,
       title: "Exercise",
       time: "4hrs",
       active: "Last Week - 5hrs",
+      color: "hsl(145, 58%, 55%)",
+      icon: "../assets/images/icon-exercise.svg",
     },
     {
       id: 4,
       title: "Social",
       time: "5hrs",
       active: "Last Week - 10hrs",
+      color: "hsl(264, 64%, 52%)",
+      icon: "../assets/images/icon-social.svg",
     },
 
     {
@@ -44,6 +54,8 @@ export class AppComponent {
       title: "Self Care",
       time: "2hrs",
       active: "Last Week - 2hrs",
+      color: "hsl(43, 84%, 65%)",
+      icon: "../assets/images/icon-self-care.svg",
     },
   ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ProfileComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
